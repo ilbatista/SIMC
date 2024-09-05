88c9dec06ca6600ae4fd5c422bb1dc3072b5a76d
Ives Lima <ives.lb@gmail.com>
2024-09-05 13:25:26 -0400

    Update SIMC.ino
    Implementados:
    - ThingSpeak
    - Sensor PMS5003

--- SIMC.ino
+++ SIMC.ino
@@ -1,59 +1,64 @@
+#include "PMS.h"
 #include "WiFi.h"
-#include "RTClib.h"
+#include "NTPClient.h"
+#include "WiFiUdp.h"
 #include "LiquidCrystal_I2C.h"
+#include "Wire.h"
+#include "RTClib.h"
 #include "DHTesp.h"
-#include <UniversalTelegramBot.h>
-#include <WiFi.h>
-#include <WiFiClientSecure.h>
+#include "Chaves.h"
+#include "ThingSpeak.h"
 
-#define POT_PIN 35 //Fixa o pino do potenciômetro que simula o detector de carbono
+#define RXD2 16
+#define TXD2 17
 #define DHT_PIN 23 //Fixa o pino do sensor DHT22
 
-#define TOKEN_TELEGRAM "7047714266:AAGrQOUfDmbzMF9ccp92DaMzE5mqTMosgYM"
-#define ID_CHAT "129467599"
+const char* ssid = SECRET_SSID; //SSID da rede Wi-fi
+const char* senha =  SECRET_PASS; //Senha da rede Wi-fi
+const long utcOffsetInSeconds = -4 * 3600;
 
-WiFiClientSecure cliente;
-UniversalTelegramBot bot(TOKEN_TELEGRAM, cliente);
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
 
-const char* SSID = "Wokwi-GUEST"; //SSID da rede Wi-fi
-const char* SENHA =  ""; //Senha da rede Wi-fi
-
-bool risco = false;
-int potInicial, pot; //Instancia as variáveis que receberão os valores do potenciômetro
+PMS pms(Serial2);
+PMS::DATA data;
 DHTesp sensor; //Instancia o objeto do sensor DHT22
 TempAndHumidity dadosIniciais, dados; //Instancia os objetos que receberão as leituras do sensor
 RTC_DS1307 rtc; //Instancia o objeto que receberá os dados do relógio
-LiquidCrystal_I2C lcd(0x27,20,4); //Instancia o display LCD
+LiquidCrystal_I2C lcd(0x27,16,2); //Instancia o display LCD
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+WiFiClient client;
 
 unsigned long agora = 0; //Cria a variável a ser usada para controlar o timer interno
 
-void setup() {
-  Serial.begin(115200); //Monitor serial para debug
+void setup()
+{
+  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  Serial.begin(115200);
+  Serial.println("Aquecendo");
 
-  pinMode(POT_PIN, INPUT); //Pino do potenciômetro
+  agora = millis(); //Timer interno recebe o momento atual
+  while(millis() < agora + 5000); //Cria um delay "non-blocking" de 1 segundo
+
   lcd.init(); //Inicia o display LCD
-  //lcd.backlight(); //Liga o backlight do display LCD
+  lcd.backlight(); //Liga o backlight do display LCD
 
-  sensor.setup(DHT_PIN, DHTesp::DHT22); //Inicia o sensor DHT22
+  sensor.setup(DHT_PIN, DHTesp::DHT11); //Inicia o sensor DHT11
 
-  WiFi.begin(SSID, SENHA); //Inicia a conexão Wi-fi
-
-  cliente.setCACert(TELEGRAM_CERTIFICATE_ROOT);
+  WiFi.begin(ssid, senha); //Inicia a conexão Wi-fi
 
   //Aguarda a conexão Wi-fi, enquanto exibe uma mensagem no display
   while (WiFi.status() != WL_CONNECTED) {
-    lcd.setCursor(0, 0);
-    lcd.println("Conectando");
+    lcd.setCursor(0,0);
+    lcd.print("Conectando");
   }
 
   //Mostra uma mensagem de conexão bem-sucedida no display
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0,0);
   lcd.print("Conectado!");
   
-  agora = millis(); //Timer interno recebe o momento atual
-  while(millis() < agora + 1000); //Cria um delay "non-blocking" de 1 segundo
-
-  printWifiData();
+  ThingSpeak.begin(client);
 
   agora = millis(); //Timer interno recebe o momento atual
   while(millis() < agora + 1000); //Cria um delay "non-blocking" de 1 segundo
@@ -65,108 +70,109 @@ void setup() {
     abort();
   }
 
+  timeClient.begin();
+  timeClient.update();
+
+  rtc.adjust(DateTime(timeClient.getEpochTime()));
+
   dadosIniciais = sensor.getTempAndHumidity(); //Recebe os dados iniciais do sensor
 
-  potInicial = map(analogRead(POT_PIN), 0, 4095, 0, 100); //Lê e mapeia o valor inicial do potenciômetro
-  
   //Exibe no display as leituras dos sensores
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.println("Temperatura: " + String(dadosIniciais.temperature, 1) + (char)223 + "C");
+  lcd.print("Temp: " + String(dadosIniciais.temperature, 1) + (char)223 + "C");
   lcd.setCursor(0,1);
-  lcd.println("Umidade: " + String(dadosIniciais.humidity, 1) + " %");
-  lcd.setCursor(0,2);
-  lcd.println("Monoxido: " + String(potInicial) + " ppm");
+  lcd.print("Umid: " + String(dadosIniciais.humidity, 1) + "%");
 }
+ 
+void loop()
+{
+  if (pms.read(data))
+  {
+    agora = millis(); //Timer interno recebe o momento atual
+    while(millis() < agora + 1000); //Cria um delay "non-blocking" de 1 segundo
 
-void loop() {
-  //delay(10); //Apenas para o simulador
+    DateTime now = rtc.now(); //Instancia um objeto data/hora e recebe o valor do RTC
 
-  agora = millis(); //Timer interno recebe o momento atual
+    dados = sensor.getTempAndHumidity(); //Recebe os dados atuais do sensor
 
-  DateTime now = rtc.now(); //Instancia um objeto data/hora e recebe o valor do RTC
+    //Condicional executado se houver alteração nos valores
+    if(dados.temperature != dadosIniciais.temperature || dados.humidity != dadosIniciais.humidity){
+      //Atualiza os dados iniciais para comparação posterior
+      dadosIniciais = sensor.getTempAndHumidity();
 
-  dados = sensor.getTempAndHumidity(); //Recebe os dados atuais do sensor
+      //Exibe no display as leituras dos sensores
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Temp: " + String(dados.temperature, 1) + (char)223 + "C");
+      lcd.setCursor(0,1);
+      lcd.print("Umid: " + String(dados.humidity, 1) + "%");
 
-  pot = map(analogRead(POT_PIN), 0, 4095, 0, 50); //Lê e mapeia o valor atual do potenciômetro
+      agora = millis(); //Timer interno recebe o momento atual
+      while(millis() < agora + 1000); //Cria um delay "non-blocking" de 1 segundo
 
-  //Condicional executado se houver alteração nos valores
-  if(dados.temperature != dadosIniciais.temperature || dados.humidity != dadosIniciais.humidity || pot != potInicial){
-    //Atualiza os dados iniciais para comparação posterior
-    dadosIniciais = sensor.getTempAndHumidity();
-    potInicial = pot;
-
-    //Exibe no display as leituras dos sensores
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.println("Temperatura: " + String(dados.temperature, 1) + (char)223 + "C");
-    lcd.setCursor(0,1);
-    lcd.println("Umidade: " + String(dados.humidity, 1) + " %");
-    lcd.setCursor(0,2);
-    lcd.println("Monoxido: " + String(pot) + " ppm");
-
-    //Condicional executado caso as leituras estejam dentro das margens de risco
-    if((dados.temperature >= 30 && dados.humidity <= 30) || pot >= 15){
-      //Exibe uma mensagem de alerta no display
-      lcd.setCursor(0,3);
-      lcd.print("Risco de incendio!");
-      risco = true;
+      lcd.setCursor(0,0);
+      lcd.print("PM2.5: " + String(data.PM_AE_UG_2_5) + "ug/m3");
+      lcd.setCursor(0,1);
+      lcd.print("PM10:  " + String(data.PM_AE_UG_10_0) + "ug/m3");
     }
 
-    if(risco == true)
-      bot.sendMessage(ID_CHAT, "Detectadas condições de risco para a ocorrência de incêndio!", "");
+    Serial.println("Data/hora atual");
+    if(now.day() < 10)
+      Serial.print('0');
+    Serial.print(now.day(), DEC);
+    Serial.print('/');
+    if(now.month() < 10)
+      Serial.print('0');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.year(), DEC);
+    Serial.print(" ");
+    if(now.hour() < 10)
+      Serial.print('0');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    if(now.minute() < 10)
+      Serial.print('0');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    if(now.second() < 10)
+      Serial.print('0');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    Serial.println();
+
+    Serial.println("Temperatura e umidade do ar");
+    Serial.println("Temperatura: " + String(dados.temperature, 1) + "°C");
+    Serial.println("Umidade: " + String(dados.humidity, 1) + " %");
+    Serial.println();
+
+    Serial.println("Qualidade do ar");
+    Serial.println("PM1.0 : " + String(data.PM_AE_UG_1_0) + "(μg/m³)");
+    Serial.println("PM2.5 : " + String(data.PM_AE_UG_2_5) + "(μg/m³)");
+    Serial.println("PM10  : " + String(data.PM_AE_UG_10_0) + "(μg/m³)");
+    Serial.println();
+    Serial.println("----------");
+    Serial.println();
+
+    ThingSpeak.setField(1, dados.temperature);
+    ThingSpeak.setField(2, dados.humidity);
+    ThingSpeak.setField(3, data.PM_AE_UG_1_0);
+    ThingSpeak.setField(4, data.PM_AE_UG_2_5);
+    ThingSpeak.setField(5, data.PM_AE_UG_10_0);
+
+    int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+
+    if(x == 200)
+      Serial.println("Channel update successful.");
+    else{
+      Serial.println("Problem updating channel. HTTP error code " + String(x));
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("HTTP error" + String(x));
+    }
+
+    agora = millis(); //Timer interno recebe o momento atual
+    while(millis() < agora + 10000); //Cria um delay "non-blocking" de 10 segundos
   }
-
-  /*
-  Serial.print("Data/hora atual: ");
-  if(now.day() < 10)
-    Serial.print('0');
-  Serial.print(now.day(), DEC);
-  Serial.print('/');
-  if(now.month() < 10)
-    Serial.print('0');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.year(), DEC);
-  Serial.print(" ");
-  if(now.hour() < 10)
-    Serial.print('0');
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  if(now.minute() < 10)
-    Serial.print('0');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  if(now.second() < 10)
-    Serial.print('0');
-  Serial.print(now.second(), DEC);
-  Serial.println();
-  */
-
-  while(millis() < agora + 1000); //Cria um delay "non-blocking" de 5 segundos
-}
-
-void printWifiData() {
-  lcd.setCursor(0,0);
-  IPAddress ip = WiFi.localIP();
-  lcd.println("Endereco IP: ");
-  lcd.setCursor(0,1);
-  lcd.println(ip);
-
-  byte mac[6];
-  lcd.setCursor(0,2);
-  WiFi.macAddress(mac);
-  lcd.println("Endereco MAC: ");
-  lcd.setCursor(0,3);
-  lcd.print(mac[5], HEX);
-  lcd.print(":");
-  lcd.print(mac[4], HEX);
-  lcd.print(":");
-  lcd.print(mac[3], HEX);
-  lcd.print(":");
-  lcd.print(mac[2], HEX);
-  lcd.print(":");
-  lcd.print(mac[1], HEX);
-  lcd.print(":");
-  lcd.println(mac[0], HEX);
 }
